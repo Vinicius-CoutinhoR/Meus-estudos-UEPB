6c16532037fd359f5c11ae988231792750723f14
Vinícius Coutinho <95356467+Vinicius-CoutinhoR@users.noreply.github.com>
2022-12-05 18:33:09 -0300

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,20 +12,5 @@ package main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Adicionando uma nova mudança no netbeans!");
-        
-        System.out.println("");
-        
-        System.out.println("Eae Diego kkkkk"
-                + ""
-                + ""
-                + "");
-        System.out.println("Nova linhaaaaaa!");
-        System.out.println("1 + 1 é: " + 1+1);
-        System.out.println("1 + 1 é: " + 1+1);
-        System.out.println("1 + 1 é: " + 1+1);
-        System.out.println("1 + 1 é: " + 1+1);
-        System.out.println("1 + 1 é: " + 1+1);
-        System.out.println("1 + 1 é: " + 1+1);
     }
 }
