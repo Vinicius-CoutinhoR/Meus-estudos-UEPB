5adccc5c857644f5e5b422532b4f4af47887402c
vinicius <vinicius@pop-os.localdomain>
2023-01-01 16:29:07 -0300

    initial commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -7,7 +7,7 @@ package main;
 
 /**
  *
- * @author Vinicius
+ * @author Vinicius Coutinho
  */
 public class Main {
     public static void main(String[] args) {
